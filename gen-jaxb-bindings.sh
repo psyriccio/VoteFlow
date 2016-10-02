0b5b03116cec2102d17c771ac1c4ecacbe526797
John Gleezowood <psyriccio@gmail.com>
2016-10-02 22:43:42 +0300

    now using custom xjc JAXB-binding compiler provided in tools directory, adding xsd-schemas used in project, regenerating jaxb-classes using new xjc compiler

--- gen-jaxb-bindings.sh
+++ gen-jaxb-bindings.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-xjc -nv -extension -mark-generated -d ./src -p psyriccio.voteflow.api.jaxb.topics -xmlschema ./src/psyriccio/voteflow/api/jaxb/topics.xsd
-xjc -nv -extension -mark-generated -d ./src -p psyriccio.voteflow.api.jaxb.classes -xmlschema ./src/psyriccio/voteflow/api/jaxb/classes.xsd
-xjc -nv -extension -mark-generated -d ./src -p psyriccio.voteflow.api.jaxb.deputies -xmlschema ./src/psyriccio/voteflow/api/jaxb/deputies.xsd
-xjc -nv -extension -mark-generated -d ./src -p psyriccio.voteflow.api.jaxb.stages -xmlschema ./src/psyriccio/voteflow/api/jaxb/stages.xsd
-xjc -nv -extension -mark-generated -d ./src -p psyriccio.voteflow.api.jaxb.votes -xmlschema ./src/psyriccio/voteflow/api/jaxb/votes.xsd
-xjc -nv -extension -mark-generated -d ./src -p psyriccio.voteflow.api.jaxb.periods -xmlschema ./src/psyriccio/voteflow/api/jaxb/periods.xsd
-xjc -nv -extension -mark-generated -d ./src -p psyriccio.voteflow.api.jaxb.federalorgans -xmlschema ./src/psyriccio/voteflow/api/jaxb/federal-organs.xsd
+java -jar ./tools/xjc.jar -nv -extension -mark-generated -d ./src -p psyriccio.voteflow.api.jaxb.topics -xmlschema ./src/psyriccio/voteflow/api/jaxb/topics.xsd
+java -jar ./tools/xjc.jar -nv -extension -mark-generated -d ./src -p psyriccio.voteflow.api.jaxb.classes -xmlschema ./src/psyriccio/voteflow/api/jaxb/classes.xsd
+java -jar ./tools/xjc.jar -nv -extension -mark-generated -d ./src -p psyriccio.voteflow.api.jaxb.deputies -xmlschema ./src/psyriccio/voteflow/api/jaxb/deputies.xsd
+java -jar ./tools/xjc.jar -nv -extension -mark-generated -d ./src -p psyriccio.voteflow.api.jaxb.stages -xmlschema ./src/psyriccio/voteflow/api/jaxb/stages.xsd
+java -jar ./tools/xjc.jar -nv -extension -mark-generated -d ./src -p psyriccio.voteflow.api.jaxb.votes -xmlschema ./src/psyriccio/voteflow/api/jaxb/votes.xsd
+java -jar ./tools/xjc.jar -nv -extension -mark-generated -d ./src -p psyriccio.voteflow.api.jaxb.periods -xmlschema ./src/psyriccio/voteflow/api/jaxb/periods.xsd
+java -jar ./tools/xjc.jar -nv -extension -mark-generated -d ./src -p psyriccio.voteflow.api.jaxb.federalorgans -xmlschema ./src/psyriccio/voteflow/api/jaxb/federal-organs.xsd
